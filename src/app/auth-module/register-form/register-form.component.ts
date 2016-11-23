ddfa72d1c6cd25dad4911359c30c19256c82cd5a
GS703 <gs703@live.mdx.ac.uk>
2016-11-23 02:43:43 +0100

    +fix register bug

--- src/app/auth-module/register-form/register-form.component.ts
+++ src/app/auth-module/register-form/register-form.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {User} from "../../shared/User";
 import {CustomAuthService} from "../services/custom-auth.service";
 import {EqualValidator} from "./equal-validator";
+import {NotifyService} from "../../shared/api/notify.service";
+import {NotificationData, NotificationTypes} from "../../shared/notification/notification/notification.model";
+import {Form} from "@angular/forms";
 
 @Component({
   selector: 'app-register-form',
@@ -13,22 +16,27 @@ export class RegisterFormComponent implements OnInit {
   public user: User = <User> {};
 
 
-  constructor(private registerAuth: CustomAuthService) {
-
-  }
+  constructor(
+      private registerAuth: CustomAuthService,
+      private notificationService:NotifyService
+  ) {}
 
   ngOnInit() {
   }
 
-  manualRegister(registerForm){
-
-    console.log("here")
-
-    let userName = registerForm.username;
-    let email = registerForm.email;
-    let password=registerForm.password;
+  manualRegister(registerForm:any){
+    debugger;
+    let userName = registerForm.value.username;
+    let email = registerForm.value.email;
+    let password=registerForm.value.password;
 
     this.registerAuth.register(userName,email,password);
+
+    this.notificationService.notify( <NotificationData> {
+      message: "Account created! :)",
+      type: NotificationTypes.SUCCESS
+    });
+
   }
 
 
