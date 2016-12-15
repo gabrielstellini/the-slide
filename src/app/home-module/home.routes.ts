c7d5fae84bc24f8fd000d86a3639832fd61e09d7
GS703 <gs703@live.mdx.ac.uk>
2016-12-15 11:15:13 +0100

    +add score route +add score link from website

--- src/app/home-module/home.routes.ts
+++ src/app/home-module/home.routes.ts
@@ -2,9 +2,11 @@ import {Routes, RouterModule} from "@angular/router";
 import {CreditsComponent} from "./credits/credits.component";
 import {GameComponent} from "./game/game.component";
 import {InstructionsComponent} from "./instructions/instructions.component";
+import {ScoresComponent} from "./scores/scores.component";
 
 export const homeRoutes : Routes = [
     {path: 'home',component: GameComponent},
     {path: 'instructions', component:InstructionsComponent},
-    {path: 'credits', component:CreditsComponent}
+    {path: 'credits', component:CreditsComponent},
+    {path: 'scores', component:ScoresComponent}
 ];
