8dac42720d861a3e8761814b67c6743250ce470b
GS703 <gs703@live.mdx.ac.uk>
2016-12-17 20:33:03 +0100

    +add interface for data +add generated table +add styles

---
+++ src/app/home-module/scores/ColumnAttrib.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by gabriel on 12/17/16.
+ */
+export interface ColumnAttrib{
+    username:string;
+    score:number;
+}
--- src/app/home-module/scores/scores.component.ts
+++ src/app/home-module/scores/scores.component.ts
@@ -1,18 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LocalUserService} from "../../shared/api/local-user.service";
+import {ColumnAttrib} from "./ColumnAttrib";
+let $ = require('../../../../node_modules/jquery/dist/jquery.min.js');
 
 @Component({
     selector: 'app-scores',
     templateUrl: './scores.component.html',
-    styleUrls: ['./scores.component.scss']
+    styleUrls: ['./scores.component.scss'],
+    providers: [LocalUserService]
 })
+
 export class ScoresComponent implements OnInit {
 
-    constructor() { }
+    private tableAttrib: Array<ColumnAttrib>;
 
-    ngOnInit() {
+    constructor(private localService:LocalUserService) {
+         this.tableAttrib = this.getUserData();
     }
 
-    gnerateTable(){
+    ngOnInit() {
+
+    }
+
+    getUserData():Array<ColumnAttrib>{
+
+        let result:Array<ColumnAttrib> = [];
+
+
+        let length = this.localService.getLastUserID();
+
+
+        for(let i=0;i<length;i++){
+            let tempUser = this.localService.getUser(i.toString());
+            let tempColumnAttrib:ColumnAttrib = <ColumnAttrib>{};
+            tempColumnAttrib.username = tempUser.username;
+            tempColumnAttrib.score = tempUser.score;
+
+            result.push(tempColumnAttrib);
+        }
+
+
+        return result;
 
     }
 
